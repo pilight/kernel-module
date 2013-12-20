1c3b69084cfa60ed5c2d040d2618fa6417ad5371
mercuri0 <mercurio@gmx.org>
2013-12-20 18:05:58 +0100

    changed from rcu to kfifo, added multiple devices
    changed from rcu to kfifo. added possiblity to configure multiple
    devices. changed some variable names for readibility.

--- test.c
+++ test.c
@@ -10,32 +10,41 @@
  
 #include <string.h>
 
-#define POLL_TIMEOUT	1000
+#define POLL_TIMEOUT				1000
+
+#define IOCTL_GPIO_IN				1
+#define IOCTL_LONGEST_V_P			2
+#define IOCTL_SHORTEST_V_P			3
+#define IOCTL_START_RECEIVER			4
+#define IOCTL_STOP_RECEIVER			5
 
 int main(void) {
 	int f = -1;
 	int i = 0;
 	int ret = 0;
+	int n = 0;
 	char buff[255] = {0};
 
-	if((f = open("/dev/pilight", O_RDONLY)) == -1) {
+	if((f = open("/dev/pilight0", O_RDONLY)) == -1) {
 		perror("open");
 		return;
 	}
-
-	// if(ioctl(f, 0, 18) != 0) {
-		// printf("cannot claim gpio %d\n", 18);
-	// }
-	// ioctl(f, 1, 150);
-	// ioctl(f, 10, 20000);
+	
+	ioctl(f, IOCTL_GPIO_IN, 23)
+	ioctl(f, IOCTL_LONGEST_V_P, 20000);
+	ioctl(f, IOCTL_SHORTEST_V_P, 150);
+	ioctl(f, IOCTL_START_RECEIVER, 0);
+	
 	while(1) {
 		ret=read(f, buff, 10);
 		if(ret < 0) {
 			perror("read()");
 			return 4;
 		}
-		printf("%d %s\n", ret, buff);
+		printf("pulse %d: %s\n", n, buff);
+		n++;
 	}	
-	
+
+	ioctl(f, IOCTL_STOP_RECEIVER, 0);
 	close(f);
 }

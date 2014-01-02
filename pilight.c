65e9069f02042cd0ba9f0ac5fa68f7f5e92f7bd7
mercuri0 <mercurio@gmx.org>
2014-01-02 01:47:04 +0100

    added transmitter. watch out! gpio pins have to be free!!

--- pilight.c
+++ pilight.c
@@ -12,33 +12,66 @@
 #include <linux/cdev.h>
 #include <linux/slab.h>
 #include <linux/kfifo.h>
+#include <linux/delay.h>
 
-#define OUTPUT_BUFFER_SIZE			4000
+#define READ_BUFFER_SIZE			4000
 #define FIFO_BUFFER_SIZE			1800
 #define GPIO_IN						23
+#define GPIO_OUT					27
 #define LONGEST_VALID_PULSE			20000
 #define SHORTEST_VALID_PULSE		150
 
-#define GPIO_PIN_DEV_STR			"pin"
-#define GPIO_PIN_STR				"pilight rf-in"
+#define GPIO_IN_PIN_DEV_STR			"in_pin"
+#define GPIO_IN_PIN_STR				"pilight rf-in"
+#define GPIO_OUT_PIN_DEV_STR		"out_pin"
+#define GPIO_OUT_PIN_STR			"pilight rf-out"
 #define DEVICE_NAME					"pilight"
+
+#define dprintk(fmt, args...) \
+do { if (debug) printk(KERN_DEBUG DEVICE_NAME ": " fmt, ## args); } while (0)
+
+#define iprintk(fmt, args...) \
+do { printk(KERN_INFO DEVICE_NAME ": " fmt, ## args); } while (0)
+
 
 #define IOCTL_GPIO_IN				10
 #define IOCTL_LONGEST_V_P			11
 #define IOCTL_SHORTEST_V_P			12
 #define IOCTL_START_RECEIVER		13
 #define IOCTL_STOP_RECEIVER			14
+#define IOCTL_FILTER_ON				15
+#define IOCTL_GPIO_OUT				16
+#define IOCTL_START_TRANSMITTER		17
+#define IOCTL_STOP_TRANSMITTER		18
 
 #define PILIGHT_DEVICES_N			1
 
-unsigned short _free_irq = 0;
-unsigned short _free_gpio = 0;
-unsigned short gpio_pin = GPIO_IN;
-unsigned short irq_gpio_pin = -1;
-char out_buf[OUTPUT_BUFFER_SIZE];
+#define FILTER_ON					0
 
-int longest_valid_pulse = LONGEST_VALID_PULSE;
-int shortest_valid_pulse = SHORTEST_VALID_PULSE;
+unsigned short debug = 0;
+
+module_param(debug, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(debug, "Turn debuging output on/off");
+
+unsigned short _free_irq = 0;
+unsigned short _free_gpio_in = 0;
+unsigned short _free_gpio_out = 0;
+
+unsigned short gpio_in_pin = -1;
+unsigned short gpio_out_pin = -1;
+unsigned short irq_gpio_pin = -1;
+
+module_param(gpio_in_pin, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(gpio_in_pin, "GPIO pin that is connected to the 433 Mhz receiver");
+
+module_param(gpio_out_pin, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(gpio_out_pin, "GPIO pin that is connected to the 433 Mhz sender");
+
+char read_buf[READ_BUFFER_SIZE];
+
+int longest_valid_pulse = -1;
+int shortest_valid_pulse = -1;
+short filter_on = -1;
 
 struct {
 	unsigned long first;
@@ -50,41 +83,44 @@ struct {
 struct timeval tv;
 unsigned char valid_buffer = 0x00;
 
-static DECLARE_WAIT_QUEUE_HEAD(fifo_list);
-static DECLARE_KFIFO_PTR(pulse_fifo, int);
+static DECLARE_WAIT_QUEUE_HEAD(fifo_in_list);
+static DECLARE_KFIFO_PTR(pulse_fifo_in, int);
 
 
 static int pilight_filter(int pulse_length) {
-  valid_buffer <<= 1;
-  
-  if(pulse_length > shortest_valid_pulse && pulse_length < longest_valid_pulse) {
+	//dprintk("pilight_filter()\n");
+	valid_buffer <<= 1;
+
+	if(pulse_length > shortest_valid_pulse && pulse_length < longest_valid_pulse) {
 		valid_buffer |= 0x01;
-		if(valid_buffer == 0xFF) {
+		if((filter_on) && (valid_buffer == 0xFF)) {
 			return pulse_length;
+		}else{
+			if((! filter_on)){
+				return pulse_length;
+			}
 		}
 	}
-  
+
 	return 0;
 }
 
 static irqreturn_t pilight_irq(int irq, void *dev_id, struct pt_regs *regs) {
 	int pulse_length = 0;
 	static int n = 0, error_n = 0;
-	
-	//printk(KERN_INFO "pilight_irq()\n");
-	
-	
+	//dprintk("pilight_irq()\n");
+
 	do_gettimeofday(&tv);
 	timestamp.first = timestamp.second;
 	timestamp.second = 1000000 * (unsigned int)tv.tv_sec + (unsigned int)tv.tv_usec;
 
 	pulse_length = pilight_filter((int)timestamp.second-(int)timestamp.first);
-	
+
 	if(pulse_length > 0) {
-		//printk(KERN_INFO "pulse %d: %d\n", n, pulse_length);
+		//dprintk("pulse %d: %d\n", n, pulse_length);
 		n++;
-		error_n = kfifo_put(&pulse_fifo, &pulse_length);
-		wake_up_interruptible(&fifo_list);
+		error_n = kfifo_put(&pulse_fifo_in, &pulse_length);
+		wake_up_interruptible(&fifo_in_list);
 	}
 
 	return IRQ_HANDLED;
@@ -96,25 +132,24 @@ static struct cdev *pilight_devices = NULL;
 static int pilight_devices_n = PILIGHT_DEVICES_N;
 
 static int pilight_open(struct inode *i, struct file *f) {
-	//printk(KERN_INFO "pilight_open()\n");
+	dprintk("pilight_open()\n");
 	return nonseekable_open(i, f);
 }
 
 static int pilight_close(struct inode *i, struct file *f) {
-	//printk(KERN_INFO "pilight_close()\n");
+	dprintk("pilight_close()\n");
 	return 0;
 }
 
 static ssize_t pilight_read(struct file *f, char __user *buf, size_t len, loff_t *off) {
-	
 	ssize_t retval = 0;
-	//unsigned int copied;
 	int pulse = 0;
 
-	wait_event_interruptible(fifo_list, kfifo_len(&pulse_fifo) != 0);
-	retval = kfifo_get(&pulse_fifo, &pulse);
-	sprintf(out_buf, "%d", pulse);
-	if(copy_to_user(buf, out_buf, len ) != 0 ){
+	//dprintk("pilight_read()\n");
+	wait_event_interruptible(fifo_in_list, kfifo_len(&pulse_fifo_in) != 0);
+	retval = kfifo_get(&pulse_fifo_in, &pulse);
+	sprintf(read_buf, "%d", pulse);
+	if(copy_to_user(buf, read_buf, len ) != 0 ){
 		retval = -EFAULT;
 	}else{
 		retval = strlen(buf);
@@ -123,66 +158,155 @@ static ssize_t pilight_read(struct file *f, char __user *buf, size_t len, loff_t
 }
 
 static ssize_t pilight_write(struct file *f, const char __user *buf, size_t len, loff_t *off) {
-	//printk(KERN_INFO "pilight_write()\n");
+	int pulse_count = 0, i;
+	int *pulse_buf;
+
+	//dprintk("pilight_write()\n");
+
+	
+	pulse_count = len / sizeof(int);
+
+	if (len % sizeof(int))
+		return -EINVAL;
+
+	pulse_buf = memdup_user(buf, len);
+
+	if (IS_ERR(pulse_buf))
+		return PTR_ERR(pulse_buf);
+
+	//disable_irq(irq_gpio_pin);
+
+	for (i = 0; i < pulse_count; i++) {
+		if (i%2){
+			//dprintk("gpio pin %d set to 0, delay: %d\n", gpio_out_pin, pulse_buf[i]);
+			gpio_set_value(gpio_out_pin,0);
+			udelay(pulse_buf[i]);
+		}
+		else{
+			//dprintk("gpio pin %d set to 1, delay: %d\n", gpio_out_pin, pulse_buf[i]);
+			gpio_set_value(gpio_out_pin,1);
+			udelay(pulse_buf[i]);
+		}
+
+	}
+	//dprintk("gpio pin %d set to 0\n", gpio_out_pin);
+	gpio_set_value(gpio_out_pin,0);
+
+	//enable_irq(irq_gpio_pin);
+
+
 	return len;
 }
 
-int pilight_init_gpio(void) {
+int pilight_init_gpio_in(void) {
 
 	int error_n = 0;
 
-	if(!gpio_is_valid(gpio_pin)) {
-		printk(KERN_INFO "gpio number %d is invalid\n", gpio_pin);
+	dprintk("starting gpio in request\n");
+
+	if(!gpio_is_valid(gpio_in_pin)) {
+		printk(KERN_INFO "gpio number %d is invalid\n", gpio_in_pin);
 		return -1;
 	}
 
-	error_n = gpio_request(gpio_pin, GPIO_PIN_STR);
+	error_n = gpio_request(gpio_in_pin, GPIO_IN_PIN_STR);
 
 	if(error_n != 0) {
-		printk(KERN_INFO "cannot claim gpio %d (error: %d)\n", gpio_pin,error_n);
-		return -1;
+		iprintk("cannot claim gpio in pin %d (error: %d)\n", gpio_in_pin,error_n);
+		return error_n;
 	}
-	_free_gpio = 1;
+	_free_gpio_in = 1;
 
-	if(gpio_direction_input(gpio_pin) < 0) {
-		printk(KERN_INFO "cannot set gpio %d as input\n", gpio_pin);
-		gpio_free(gpio_pin);
-		return -1;
-	}
-	
-	if((irq_gpio_pin = gpio_to_irq(gpio_pin)) < 0) {
-		printk(KERN_INFO "gpio to irq mapping failure %d\n", gpio_pin);
-		gpio_free(gpio_pin);
+	if(gpio_direction_input(gpio_in_pin) < 0) {
+		iprintk("cannot set gpio pin %d as input\n", gpio_in_pin);
+		gpio_free(gpio_in_pin);
+		_free_gpio_in = 0;
 		return -1;
 	}	
-	
-	if(request_irq(irq_gpio_pin, (irq_handler_t)pilight_irq, IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING, GPIO_PIN_STR, GPIO_PIN_DEV_STR) < 0) {
-		printk(KERN_INFO "cannot request irq for gpio %d\n", gpio_pin);
+
+	dprintk("gpio in request done, starting irq request\n");
+
+	if((irq_gpio_pin = gpio_to_irq(gpio_in_pin)) < 0) {
+		iprintk("gpio to irq mapping failure %d\n", gpio_in_pin);
+		gpio_free(gpio_in_pin);
+		_free_gpio_in = 0;
+		return -1;
+	}
+
+	dprintk("irq %d mapped to gpio pin %d\n", irq_gpio_pin, gpio_in_pin);
+
+	if(request_irq(irq_gpio_pin, (irq_handler_t)pilight_irq, IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING, GPIO_IN_PIN_STR, GPIO_IN_PIN_DEV_STR) < 0) {
+		iprintk("cannot request irq for gpio %d\n", gpio_in_pin);
 		return -1;
 	}
 	disable_irq(irq_gpio_pin);
 	_free_irq = 1;
 
-	printk(KERN_INFO "successfully requested irq %d for gpio %d\n", irq_gpio_pin, gpio_pin);
+	dprintk("successfully requested irq %d for gpio %d\n", irq_gpio_pin, gpio_in_pin);
 	return 0;
 }
 
-static void pilight_deinit_gpio(void) {
+int pilight_init_gpio_out(void) {
+
+	int error_n = 0;
+
+	if(!gpio_is_valid(gpio_out_pin)) {
+		printk(KERN_INFO "gpio number %d is invalid\n", gpio_out_pin);
+		return -1;
+	}
+
+	error_n = gpio_request(gpio_out_pin, GPIO_OUT_PIN_STR);
+
+	if(error_n != 0) {
+		iprintk("cannot claim gpio out pin %d (error: %d)\n", gpio_out_pin,error_n);
+		return error_n;
+	}
+	_free_gpio_out = 1;
+
+	if(gpio_direction_output(gpio_out_pin,1) < 0) {
+		iprintk("cannot set gpio pin %d as output\n", gpio_out_pin);
+		gpio_free(gpio_out_pin);
+		_free_gpio_out = 0;
+		return -1;
+	}
+
+	gpio_set_value(gpio_out_pin,0);
+	
+	dprintk("successfully set gpio %d pin as output\n", gpio_out_pin);
+	return 0;
+}
+
+static void pilight_deinit_gpio_in(void) {
+	dprintk("pilight_deinit_gpio_in()\n");
 	if(_free_irq) {
-		free_irq(irq_gpio_pin, GPIO_PIN_DEV_STR);
+		dprintk("free gpio in irq\n");
+		free_irq(irq_gpio_pin, GPIO_IN_PIN_DEV_STR);
 		_free_irq = 0;
 	}
-	if(_free_gpio) {
-		gpio_free(gpio_pin);
-		_free_gpio = 0;
+	if(_free_gpio_in) {
+		dprintk("free gpio in\n");
+		gpio_free(gpio_in_pin);
+		_free_gpio_in = 0;
+	}
+}
+
+static void pilight_deinit_gpio_out(void) {
+	dprintk("pilight_deinit_gpio_out()\n");
+	if(_free_gpio_out) {
+		dprintk("free gpio out\n");
+		gpio_free(gpio_out_pin);
+		_free_gpio_out = 0;
 	}
 }
 
 static long pilight_ioctl(struct file *f, unsigned int name, unsigned long val) {
-	//printk(KERN_INFO "pilight_ioctl()\n");
-	 switch(name) {
+	dprintk("pilight_ioctl()\n");
+	switch(name) {
 		case IOCTL_GPIO_IN:
-			gpio_pin = (int)val;
+			gpio_in_pin = (int)val;
+			break;
+		case IOCTL_GPIO_OUT:
+			gpio_out_pin = (int)val;
 			break;
 		case IOCTL_LONGEST_V_P:
 			longest_valid_pulse = (int)val;
@@ -190,19 +314,66 @@ static long pilight_ioctl(struct file *f, unsigned int name, unsigned long val) 
 		case IOCTL_SHORTEST_V_P:
 			shortest_valid_pulse = (int)val;
 			break;
+		case IOCTL_FILTER_ON:
+			filter_on = (int)val;
+			break;
 		case IOCTL_START_RECEIVER:
-			printk(KERN_INFO "starting receiver with gpio pin: %d, shortest valid pulse: %d, longest valid pulse: %d\n",gpio_pin,shortest_valid_pulse,longest_valid_pulse);
-			if(pilight_init_gpio() != 0) {
+			dprintk("starting receiver\n");
+			if(gpio_in_pin == -1){
+				iprintk("gpio in pin not set!\n");
 				return -1;
+			}
+			if(shortest_valid_pulse == -1){
+				iprintk("shortest valid pulse not set! setting to default (%d)\n", SHORTEST_VALID_PULSE);
+				shortest_valid_pulse = SHORTEST_VALID_PULSE;
+			}
+			if(longest_valid_pulse == -1){
+				iprintk("longest valid pulse not set! setting to default (%d)\n", LONGEST_VALID_PULSE);
+				longest_valid_pulse = LONGEST_VALID_PULSE;
+			}
+			if(filter_on == -1){
+				iprintk("attiny prefilter status not set! setting to default (%d)\n", FILTER_ON);
+				filter_on = FILTER_ON;
+			}
+			dprintk("starting receiver initialization\n");
+
+			if(pilight_init_gpio_in() != 0) {
+				pilight_deinit_gpio_in();
+				return -1;
+			}
+
+			if(!filter_on){
+				iprintk("started receiver without kernel prefilter, gpio pin: %d, shortest valid pulse: %d, longest valid pulse: %d\n",gpio_in_pin,shortest_valid_pulse,longest_valid_pulse);
+			}else{
+				iprintk("started receiver with kernel prefilter, gpio pin: %d, shortest valid pulse: %d, longest valid pulse: %d\n",gpio_in_pin,shortest_valid_pulse,longest_valid_pulse);
 			}
 			enable_irq(irq_gpio_pin);
 			break;
 		case IOCTL_STOP_RECEIVER:
-			printk(KERN_INFO "stopping receiver\n");
+			dprintk("stopping receiver\n");
 			disable_irq(irq_gpio_pin);
-			pilight_deinit_gpio();
+			pilight_deinit_gpio_in();
+			iprintk("stopped receiver\n");
+
 			break;
-			 
+		case IOCTL_START_TRANSMITTER:
+			dprintk("starting transmitter\n");
+			if(gpio_out_pin == -1){
+				iprintk("gpio out pin not set!\n");
+				return -1;
+			}
+
+			if(pilight_init_gpio_out() != 0) {
+				pilight_deinit_gpio_out();
+				return -1;
+			}
+			iprintk("started transmitter, gpio pin: %d\n",gpio_out_pin);
+			break;
+		case IOCTL_STOP_TRANSMITTER:
+			dprintk("stopping transmitter\n");
+			pilight_deinit_gpio_out();
+			iprintk("stopped transmitter\n");
+			break;
 		default:
 			return -1;
 	}
@@ -222,23 +393,23 @@ static int pilight_consturct_device(struct cdev *cdevice, int minor, struct clas
 	int error_n;
 	dev_t device_n = MKDEV(major_device_num, minor);
 	struct device *device = NULL;
-	
+
 	cdev_init(cdevice, &pugs_fops);
-	
+
 	error_n = cdev_add(cdevice, device_n, 1);
 	if(error_n) {
-		printk(KERN_WARNING "error %d while trying to add %s%d", error_n, DEVICE_NAME, minor);
+		iprintk("error %d while trying to add %s%d", error_n, DEVICE_NAME, minor);
 		return error_n;
 	}
-	
+
 	device = device_create(cl,NULL,device_n,NULL,DEVICE_NAME "%d",minor);
 	if(IS_ERR(device)){
-		printk(KERN_WARNING "error %d while trying to create %s%d", error_n, DEVICE_NAME, minor);
+		iprintk("error %d while trying to create %s%d", error_n, DEVICE_NAME, minor);
 		error_n=PTR_ERR(device);
 		cdev_del(cdevice);
 		return error_n;
 	}
-	
+
 	return 0;
 }
 
@@ -250,60 +421,60 @@ static void pilight_destroy_device(struct cdev *cdevice, int minor, struct class
 
 static void pilight_cleanup(int devices_to_destroy){
 	int i;
-	
+
 	if(pilight_devices){
 		for(i = 0; i < devices_to_destroy; i++){
 			pilight_destroy_device(&pilight_devices[i], i, cl);
 		}
 		kfree(pilight_devices);
 	}
-	
+
 	if(cl)
 	{
 		class_destroy(cl);
 	}
-	
+
 	unregister_chrdev_region(MKDEV(major_device_num, 0), pilight_devices_n);
-	pilight_deinit_gpio();
+	pilight_deinit_gpio_in();
+	pilight_deinit_gpio_out();
 }
 
 static int __init pilight_init(void) {
-	//printk(KERN_INFO "%s\n", __func__);
 	int error_n = 0, i = 0;
 	int devices_to_destroy = 0;
-	
 	dev_t dev = 0;
-	
-	memset(out_buf, '\0', OUTPUT_BUFFER_SIZE);
-	
-	error_n = kfifo_alloc(&pulse_fifo, FIFO_BUFFER_SIZE, GFP_KERNEL);
-	
+
+	dprintk("%s\n", __func__);
+
+	memset(read_buf, '\0', READ_BUFFER_SIZE);
+
+	error_n = kfifo_alloc(&pulse_fifo_in, FIFO_BUFFER_SIZE, GFP_KERNEL);
+
 	if(error_n){
-		printk(KERN_ERR "error on fifo allocation\n");
+		iprintk("error on fifo allocation\n");
 		return error_n;
 	}
-	
-	
+
+
 	if(pilight_devices_n <= 0) {
-		printk(KERN_INFO "invalid number of pilight devices (%d)\n", pilight_devices_n);
+		iprintk("invalid number of pilight devices (%d)\n", pilight_devices_n);
 		return -EINVAL;
 	}
-	
+
 	if(alloc_chrdev_region(&dev, 0, pilight_devices_n, DEVICE_NAME) < 0) {
-		printk(KERN_INFO "failed to allocate pilight %d devices\n", pilight_devices_n);
-		pilight_deinit_gpio();
+		iprintk("failed to allocate pilight %d devices\n", pilight_devices_n);
 		return -1;
 	}
-	
+
 	major_device_num = MAJOR(dev);
-	
+
 	cl = class_create(THIS_MODULE, DEVICE_NAME);
 	if(IS_ERR(cl)) {
-		printk(KERN_INFO "failed to create pilight device\n");
+		iprintk("failed to create pilight device\n");
 		pilight_cleanup(devices_to_destroy);
 		return PTR_ERR(cl);
 	}
-	
+
 	pilight_devices = (struct cdev *)kzalloc(pilight_devices_n * sizeof(struct cdev), GFP_KERNEL);
 	if(!pilight_devices){
 		pilight_cleanup(devices_to_destroy);
@@ -318,18 +489,18 @@ static int __init pilight_init(void) {
 			return error_n;
 		}
 	}
-	
-	printk(KERN_INFO "pilight device driver registered, major %d\n",major_device_num);
-	
+
+	iprintk("pilight device driver registered, major %d\n",major_device_num);
+
 	return 0;
 }
 
 static void __exit pilight_exit(void) {
 
 	pilight_cleanup(pilight_devices_n);
-	kfifo_free(&pulse_fifo);
+	kfifo_free(&pulse_fifo_in);
 
-	//printk(KERN_INFO "%s\n", __func__);
+	dprintk("%s\n", __func__);
 
 }
 
@@ -337,5 +508,5 @@ module_init(pilight_init);
 module_exit(pilight_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("CurlyMoo & mercurio.");
+MODULE_AUTHOR("CurlyMoo & mercuri0");
 MODULE_DESCRIPTION("pilight rf-filter module");
--- test.c
+++ test.c
@@ -3,11 +3,11 @@
 #include <signal.h>
 #include <poll.h>
 #include <unistd.h>
- 
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
- 
+
 #include <string.h>
 
 #define POLL_TIMEOUT				1000
@@ -17,34 +17,75 @@
 #define IOCTL_SHORTEST_V_P			12
 #define IOCTL_START_RECEIVER		13
 #define IOCTL_STOP_RECEIVER			14
+#define IOCTL_FILTER_ON				15
+#define IOCTL_GPIO_OUT				16
+#define IOCTL_START_TRANSMITTER		17
+#define IOCTL_STOP_TRANSMITTER		18
+
+volatile sig_atomic_t stop;
+
+void inithand(int signum){
+	stop = 1;
+}
 
 int main(void) {
 	int f = -1;
-	int i = 0;
+	size_t code_len = 0;
 	int ret = 0;
 	int n = 0;
 	char buff[255] = {0};
+
+	int code[255] = { 300, 300, 300, 1000, 300, 300, 300, 1000 };
+	
+	while(code[code_len]) {
+		code_len++;
+	}
+	code_len++;
+	code_len*=sizeof(int);
+
+	if((f = open("/dev/pilight0", O_WRONLY)) == -1) {
+		perror("open");
+		return;
+	}
+
+	ioctl(f, IOCTL_GPIO_OUT, 22);
+	ioctl(f, IOCTL_START_TRANSMITTER, 0);
+	
+	ret = write(f, code, code_len);
+	printf("%d %d\n", ret, code_len);	
+
+	if(ret < 0) {
+		perror("read()");
+		return 4;
+	}
+
+	ioctl(f, IOCTL_STOP_TRANSMITTER, 0);
+	
+	close(f);
 
 	if((f = open("/dev/pilight0", O_RDONLY)) == -1) {
 		perror("open");
 		return;
 	}
 	
-	ioctl(f, IOCTL_GPIO_IN, 23)
-	ioctl(f, IOCTL_LONGEST_V_P, 20000);
-	ioctl(f, IOCTL_SHORTEST_V_P, 150);
+	ioctl(f, IOCTL_GPIO_IN, 23);
+	ioctl(f, IOCTL_LONGEST_V_P, 40000);
+	ioctl(f, IOCTL_SHORTEST_V_P, 50);
+	ioctl(f, IOCTL_FILTER_ON, 0);
 	ioctl(f, IOCTL_START_RECEIVER, 0);
 	
-	while(1) {
+	while(! stop) {
 		ret=read(f, buff, 10);
 		if(ret < 0) {
 			perror("read()");
 			return 4;
 		}
-		printf("pulse %d: %s\n", n, buff);
+		printf("%s\n", buff);
 		n++;
 	}	
 
 	ioctl(f, IOCTL_STOP_RECEIVER, 0);
 	close(f);
+
+	return 0;
 }
